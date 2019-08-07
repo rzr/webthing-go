629bbb4ba69d7b5bb3d944c57bf6a296de12b56d
Philippe Coval <p.coval@samsung.com>
2019-08-07 19:14:49 +0200

    example: Add single-thing
    Change-Id: Id9b3e7c018bfb5d67b611d24e49edd2adb06ecfd
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ example/single-thing.go
@@ -0,0 +1,26 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+// Copyright: 2019-present Samsung Electronics Co., Ltd. and other contributors
+// SPDX-License-Identifier: MPL-2.0
+
+package main
+
+import (
+	webthing ".."
+)
+
+func main() {
+	thing := webthing.NewThing(
+		"urn:dev:ops:my-lamp-1234",
+		"My Lamp",
+		[]string{"OnOffSwitch", "Light"},
+		"A web connected lamp")
+
+	onProperty := webthing.NewProperty("on", "boolean", false, nil)
+	thing.AddProperty(onProperty)
+
+	brightnessProperty := webthing.NewProperty("brightness", "integer", 0, nil)
+	thing.AddProperty(brightnessProperty)
+
+	server := webthing.NewServer(thing, 8888)
+	server.Start()
+}
